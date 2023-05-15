58c2aa9e403a8d75a482ad891928fcc3a9e89298
Fran Lavayen <franlavayen1@gmail.com>
2023-05-15 13:49:56 -0300

    first commit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,22 @@
+import Card from './Card';
+
+export default function Cards({ characters }) {
+   return (
+      <div>
+         {
+            characters.map((cartas) => {
+               return (
+                  <Card 
+                  name={cartas.name} 
+                  status={cartas.status} 
+                  species={cartas.species} 
+                  gender={cartas.gender} 
+                  origin={cartas.origin} 
+                  image={cartas.image}
+                  onClose={() => window.alert('Emulamos que se cierra la card')} />
+               )
+            })
+         }
+      </div>
+   )
+}
