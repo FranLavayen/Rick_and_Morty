49ad59aed19a45e2eaaf98e89caf1f16a02e8e27
Fran Lavayen <franlavayen1@gmail.com>
2023-05-16 14:00:49 -0300

    02 stilos

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,13 +1,20 @@
+import styles from "./Card.module.css"
+
 export default function Card({ name, status, species, gender, origin, image, onClose}) {
    return (
-      <div>
+      <div className={styles.conteiner}>
+      <div className={styles.conteiner.button}>
          <button onClick={onClose}>X</button>
+        
+        <div>
          <h2>{name}</h2>
          <h2>{status}</h2>
          <h2>{species}</h2>
          <h2>{gender}</h2>
          <h2>{origin.name}</h2>
          <img src={image} alt='imagen' />
+      </div>
+      </div>
       </div>
    );
 }
